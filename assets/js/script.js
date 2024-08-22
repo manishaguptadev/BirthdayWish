6db1b91a26043480237c87175b8bdecb42745146
Deepak Prasad <145206762+deepakprasaddev@users.noreply.github.com>
2024-08-22 22:42:06 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-let countDateTime = new Date("August 23, 2024 12:00:00").getTime();
+let countDateTime = new Date("August 23, 2024 00:00:00").getTime();
 
 let birthdayCounter = setInterval(() => {
   let now = new Date().getTime();
